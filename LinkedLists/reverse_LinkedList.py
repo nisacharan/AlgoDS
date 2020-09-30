03b754e688d3494a52995910f911ddedde4dc522
Sai Charan Nivarthi <saicharan.nivarthi@insideview.com>
2020-10-01 01:20:53 +0530

    added LL reversing

---
+++ LinkedLists/reverse_LinkedList.py
@@ -0,0 +1,12 @@
+def reverseList(head):
+    # Code here
+    prevNode = None
+    currNode = head
+    nextNode = None
+    while(currNode!=None):
+        nextNode = currNode.next
+        currNode.next = prevNode
+        prevNode = currNode
+        currNode = nextNode
+    head = prevNode
+    return head
