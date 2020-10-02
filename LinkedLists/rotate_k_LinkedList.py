e1225ade34b47de350407e3cc02240eb6cc14eee
Sai Charan Nivarthi <saicharan.nivarthi@insideview.com>
2020-10-02 22:15:30 +0530

    add rotatelist

---
+++ LinkedLists/rotate_k_LinkedList.py
@@ -0,0 +1,18 @@
+# This function should rotate list counter-
+# clockwise by k and return head node
+def rotateList(head, k):
+    # code here
+    currPntr = head
+    #connecting last & first nodes
+    while(currPntr.next!=None):
+        currPntr=currPntr.next
+    currPntr.next = head
+    
+    #go to kth node & break the chain
+    currPntr = head
+    while(k-1):
+        currPntr = currPntr.next
+        k-=1
+    newHead = currPntr.next
+    currPntr.next = None
+    return newHead
